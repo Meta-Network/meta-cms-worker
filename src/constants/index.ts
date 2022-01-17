4a8109ce131ea6f8a609ee8701657018ddfa65b2
Garfield550 <openpgp@550.moe>
2022-01-17 11:27:31 +0000

    feat: update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,11 @@
 import path from 'path';
 
 export const TEMPLATE_PATH = path.resolve('/opt/MetaNetwork/Template');
+export const WORKSPACE_PATH = path.resolve('/opt/MetaNetwork/Workspace');
+
+export const DEFAULT_HEXO_AVATAR_URL =
+  'https://ipfs.fleek.co/ipfs/bafybeiccss3figrixd5qhhv6i6zhbz5chmyls6ja5kscu6drg7fnjcnxgm';
+export const DEFAULT_HEXO_LANGUAGE = 'en';
+export const DEFAULT_HEXO_TIMEZONE = 'Asia/Shanghai';
+export const DEFAULT_HEXO_PUBLIC_URL = 'https://example.com';
+export const DEFAULT_HEXO_CONFIG_FILE_NAME = '_config.yml';
