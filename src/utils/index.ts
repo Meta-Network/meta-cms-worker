aa53064d4e4b631ef78f9cf9b5e9b3296f007c89
Garfield550 <openpgp@550.moe>
2022-01-17 11:27:00 +0000

    feat: add yaml utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
 // import { URL } from 'url';
+import fs from 'fs/promises';
+import yaml from 'yaml';
 
 export const isEmptyObj = (obj: Record<string, unknown>): boolean => {
   return obj && Object.keys(obj).length === 0 && obj.constructor === Object;
@@ -18,4 +20,18 @@ export function omitObj<T = Record<string, unknown>>(
   obj = { ...obj };
   props.forEach((prop) => delete obj[prop]);
   return obj;
+}
+
+export async function objectToYamlFile(
+  obj: unknown,
+  path: string,
+): Promise<void> {
+  const yamlStr = yaml.stringify(obj);
+  const data = new Uint8Array(Buffer.from(yamlStr));
+  await fs.writeFile(path, data, { encoding: 'utf8' });
+}
+export async function yamlFileToObject<T = unknown>(path: string): Promise<T> {
+  const confData = await fs.readFile(path, 'utf8');
+  const data = yaml.parse(confData);
+  return data;
 }
