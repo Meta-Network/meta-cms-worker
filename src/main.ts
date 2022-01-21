81be7478c50c4a904ac6cb38149fceb0abf5e0e7
Garfield550 <openpgp@550.moe>
2022-01-21 10:39:16 +0000

    refactor: move get task config to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import assert from 'assert';
 import { CronJob } from 'cron';
 
 import { getBackendService } from './api';
-import { logger } from './logger';
-import { startGitTask } from './task';
+import { logger, loggerService } from './logger';
+import { startWorkerTask } from './task';
+import { MixedTaskConfig } from './types';
+import { isEmptyObj } from './utils';
 import { CPUUtils } from './utils/cpu';
 
 async function bootstrap(): Promise<void> {
@@ -12,7 +15,13 @@ async function bootstrap(): Promise<void> {
   logger.info('App started');
   const cpuPct = cpu.getCPUUsage();
   logger.debug(`CPU percentage is ${cpuPct}`);
+
   await http.reportWorkerTaskStartedToBackend();
+  const taskConf = await http.getWorkerTaskFromBackend<MixedTaskConfig>();
+  assert(
+    !isEmptyObj(taskConf),
+    new Error('Can not get task config from backend or gateway'),
+  );
 
   const healthCheck = new CronJob('*/5 * * * * *', async () => {
     await http.reportWorkerTaskHealthStatusToBackend();
@@ -21,7 +30,9 @@ async function bootstrap(): Promise<void> {
   });
   healthCheck.start();
 
-  await startGitTask();
+  await startWorkerTask(taskConf);
+  await http.reportWorkerTaskFinishedToBackend();
+  loggerService.final('Task finished');
 }
 
 bootstrap();
